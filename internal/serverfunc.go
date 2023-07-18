0d45b77176cceb3a0e7da1a89e882ad56b8ef7b5
Lev <lew1968@list.ru>
2023-07-19 01:20:20 +0300

    iter1-исправил7

--- internal/serverfunc.go
+++ internal/serverfunc.go
@@ -1,11 +1,45 @@
 package internal
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"os"
+
 	"strconv"
 	"strings"
 )
+
+// ResponseRecorderWithLog is a custom implementation of http.ResponseWriter
+// that records the response data and writes it to the log.
+type ResponseRecorderWithLog struct {
+	http.ResponseWriter
+	buffer *bytes.Buffer
+}
+
+func NewResponseRecorderWithLog(w http.ResponseWriter) *ResponseRecorderWithLog {
+	return &ResponseRecorderWithLog{
+		ResponseWriter: w,
+		buffer:         bytes.NewBuffer(nil),
+	}
+}
+
+func (rw *ResponseRecorderWithLog) Write(data []byte) (int, error) {
+	rw.buffer.Write(data)
+	return rw.ResponseWriter.Write(data)
+}
+
+func (rw *ResponseRecorderWithLog) WriteHeader(statusCode int) {
+	rw.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rw *ResponseRecorderWithLog) Flush() {
+	// Log the response data here
+	responseDump := rw.buffer.String()
+	log.Printf("Ответ:\n%s\n", responseDump)
+}
 
 func HandleUpdate(storage *MemStorage) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -13,6 +47,15 @@ func HandleUpdate(storage *MemStorage) http.HandlerFunc {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
+
+		file, err1 := os.OpenFile("server1.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err1 != nil {
+			log.Fatal("Не удалось открыть файл логов:", err1)
+		}
+		defer file.Close()
+
+		// Настройка логирования для вывода в файл
+		log.SetOutput(file)
 
 		// Извлекаем параметры из формы запроса
 		metricType := r.FormValue("metricType")
@@ -52,9 +95,22 @@ func HandleUpdate(storage *MemStorage) http.HandlerFunc {
 			return
 		}
 
+		// Логирование запроса
+		requestDump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			log.Printf("Ошибка при логировании запроса: %v", err)
+		} else {
+			log.Printf("Запрос:\n%s\n", requestDump)
+		}
+
+		// Создаем ResponseRecorder для записи ответа
+		recorder := NewResponseRecorderWithLog(w)
+
 		// Обрабатываем полученные метрики
 		storage.SaveMetric(metricType, metricName, metricValue)
 
+		// Отправляем ответ клиенту
+		recorder.Flush()
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintln(w, "Metric received")
 	}
