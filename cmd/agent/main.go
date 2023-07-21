180cdedc8a8b04de1719e4723a892fec07434f57
Lev <lew1968@list.ru>
2023-07-21 13:12:15 +0300

    iter1-проба

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,20 +1,13 @@
 package main
 
 import (
+	"math/rand"
+
 	"fmt"
 	"net/http"
 	"runtime"
-	"sync/atomic"
 	"time"
 )
-
-const (
-	pollInterval   = 2 * time.Second
-	reportInterval = 10 * time.Second
-	serverAddress  = "http://localhost:8080"
-)
-
-var pollCount uint64 // Счётчик типа uint64 (беззнаковый int)
 
 type Metric struct {
 	Type  string      `json:"type"`
@@ -22,69 +15,96 @@ type Metric struct {
 	Value interface{} `json:"value"`
 }
 
-func collectMetrics() map[string]interface{} {
-	metrics := make(map[string]interface{})
-	var memStats runtime.MemStats
-	runtime.ReadMemStats(&memStats)
+func collectMetrics(pollInterval time.Duration, serverURL string) <-chan []*Metric {
+	metricsChan := make(chan []*Metric)
 
-	metrics["Alloc"] = memStats.Alloc
-	// Добавьте остальные метрики памяти, которые вам интересны
+	// Переменная для счетчика обновлений метрик
+	pollCount := 0
 
-	// Увеличиваем счётчик PollCount при каждом вызове collectMetrics
-	atomic.AddUint64(&pollCount, 1)
+	go func() {
+		for {
+			var metrics []*Metric
 
-	return metrics
+			var memStats runtime.MemStats
+			runtime.ReadMemStats(&memStats)
+
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "Alloc", Value: float64(memStats.Alloc)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "BuckHashSys", Value: float64(memStats.BuckHashSys)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "Frees", Value: float64(memStats.Frees)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "GCCPUFraction", Value: float64(memStats.GCCPUFraction)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "GCSys", Value: float64(memStats.GCSys)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "HeapAlloc", Value: float64(memStats.HeapAlloc)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "HeapIdle", Value: float64(memStats.HeapIdle)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "HeapInuse", Value: float64(memStats.HeapInuse)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "HeapObjects", Value: float64(memStats.HeapObjects)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "HeapReleased", Value: float64(memStats.HeapReleased)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "HeapSys", Value: float64(memStats.HeapSys)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "LastGC", Value: float64(memStats.LastGC)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "Lookups", Value: float64(memStats.Lookups)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "MCacheInuse", Value: float64(memStats.MCacheInuse)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "MCacheSys", Value: float64(memStats.MCacheSys)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "MSpanInuse", Value: float64(memStats.MSpanInuse)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "MSpanSys", Value: float64(memStats.MSpanSys)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "Mallocs", Value: float64(memStats.Mallocs)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "NextGC", Value: float64(memStats.NextGC)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "NumForcedGC", Value: float64(memStats.NumForcedGC)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "NumGC", Value: float64(memStats.NumGC)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "OtherSys", Value: float64(memStats.OtherSys)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "PauseTotalNs", Value: float64(memStats.PauseTotalNs)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "StackInuse", Value: float64(memStats.StackInuse)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "StackSys", Value: float64(memStats.StackSys)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "Sys", Value: float64(memStats.Sys)})
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "TotalAlloc", Value: float64(memStats.TotalAlloc)})
+
+			// Добавляем метрику RandomValue типа gauge с произвольным значением
+			randomValue := rand.Float64()
+			metrics = append(metrics, &Metric{Type: "gauge", Name: "RandomValue", Value: randomValue})
+
+			// Добавляем метрику PollCount типа counter
+			metrics = append(metrics, &Metric{Type: "counter", Name: "PollCount", Value: pollCount})
+
+			// Увеличиваем счетчик обновлений метрик
+			pollCount++
+
+			metricsChan <- metrics
+			time.Sleep(pollInterval)
+		}
+	}()
+
+	return metricsChan
 }
 
-func sendMetrics(metrics map[string]interface{}) {
-	for name, value := range metrics {
-		url := fmt.Sprintf("%s/update/gauge/%s/%v", serverAddress, name, value)
-		// Здесь мы форматируем URL, чтобы соответствовать указанному формату данных.
+func sendDataToServer(serverURL string, metrics []*Metric) {
 
-		req, err := http.NewRequest("POST", url, nil)
+	for _, metric := range metrics {
+		serverURL := fmt.Sprintf("http://localhost:8080/update/%s/%s/%v", metric.Type, metric.Name, metric.Value)
+		// Отправка POST-запроса
+		resp, err := http.Post(serverURL, "text/plain", nil)
 		if err != nil {
-			fmt.Println("Ошибка при создании запроса:", err)
-			continue
-		}
-
-		req.Header.Set("Content-Type", "text/plain")
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			fmt.Println("Ошибка при выполнении запроса:", err)
-			continue
+			fmt.Println("Ошибка при отправке запроса:", err)
+			return
 		}
 		defer resp.Body.Close()
 
-		// Опционально: можно обработать ответ от сервера, если это необходимо.
-		// respBody, err := ioutil.ReadAll(resp.Body)
-		// if err != nil {
-		// 	fmt.Println("Ошибка при чтении ответа:", err)
-		// 	continue
-		// }
-		// fmt.Println("Ответ сервера:", string(respBody))
+		// Вывод ответа от сервера
+		fmt.Println("Статус код ответа:", resp.Status)
+
 	}
 }
 
 func main() {
+	pollInterval := 2 * time.Second
+	serverURL := "http://localhost:8080/update/collect_metrics"
+
+	metricsChan := collectMetrics(pollInterval, serverURL)
+
 	for {
-		startTime := time.Now()
-
-		// Собираем метрики
-		metrics := collectMetrics()
-
-		// Добавляем PollCount и RandomValue
-		metrics["PollCount"] = atomic.LoadUint64(&pollCount)
-		metrics["RandomValue"] = 42 // Произвольное значение, замените на свою логику
-
-		// Отправляем метрики на сервер
-		sendMetrics(metrics)
-
-		// Ожидаем до следующего опроса и отправки
-		sleepTime := reportInterval - time.Since(startTime)
-		if sleepTime > 0 {
-			time.Sleep(sleepTime)
+		select {
+		case metrics := <-metricsChan:
+			sendDataToServer(serverURL, metrics)
+		case <-time.After(pollInterval):
+			fmt.Println("Сбор метрик...")
 		}
+
 	}
 }
