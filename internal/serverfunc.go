79f7fbe6dd9e68942e3f3cd3114d79ac80aba3dc
Lev <lew1968@list.ru>
2023-07-25 21:49:40 +0300

    iter3-frw

--- internal/serverfunc.go
+++ internal/serverfunc.go
@@ -69,6 +69,7 @@ func HandleUpdate(storage *MemStorage) gin.HandlerFunc {
 				if isInteger(path[4]) {
 
 					c.JSON(http.StatusOK, gin.H{"message": "StatusOK"})
+					c.String(http.StatusOK, fmt.Sprintf("%v", path[4]))
 
 					storage.SaveMetric(path[2], path[3], num1)
 
@@ -104,6 +105,8 @@ func HandleUpdate(storage *MemStorage) gin.HandlerFunc {
 				if _, err := strconv.ParseFloat(path[4], 64); err == nil {
 
 					c.JSON(http.StatusOK, gin.H{"message": "StatusOK"})
+					c.String(http.StatusOK, fmt.Sprintf("%v", path[4]))
+
 					storage.SaveMetric(path[2], path[3], num)
 
 					return
@@ -115,6 +118,8 @@ func HandleUpdate(storage *MemStorage) gin.HandlerFunc {
 
 				if _, err := strconv.ParseInt(path[4], 10, 64); err == nil {
 					c.JSON(http.StatusOK, gin.H{"message": "StatusOK"})
+					c.String(http.StatusOK, fmt.Sprintf("%v", path[4]))
+
 					storage.SaveMetric(path[2], path[3], num)
 
 					return
@@ -127,6 +132,8 @@ func HandleUpdate(storage *MemStorage) gin.HandlerFunc {
 			}
 
 			c.JSON(http.StatusOK, gin.H{"message": "StatusOK"})
+			c.String(http.StatusOK, fmt.Sprintf("%v", path[4]))
+
 			//================================================================================
 		case http.MethodGet:
 
@@ -178,7 +185,7 @@ func HandleUpdate(storage *MemStorage) gin.HandlerFunc {
 					c.JSON(http.StatusNotFound, gin.H{"error": "StatusNotFound"})
 
 				}
-
+				println("lengpath finish", lengpath)
 				if lengpath != 4 {
 					c.JSON(http.StatusNotFound, gin.H{"error": "StatusNotFound"})
 
