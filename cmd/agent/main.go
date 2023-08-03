9ef60162ccc961e2192b61c2d7a5f58f10cfb3fa
Lev <lew1968@list.ru>
2023-08-03 21:38:24 +0300

    iter5

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"net/http"
-	"net/url"
-	"os"
-	"strconv"
+
 	"time"
 
 	"project.com/internal"
@@ -29,63 +26,22 @@ func sendDataToServer(metrics []*internal.Metric, serverURL string) {
 }
 
 func main() {
-
-	var (
-		reportSeconds int
-		pollSeconds   int
-		addr          string
-	)
-
-	// Чтение переменных окружения или установка значений по умолчанию
-	addrEnv := os.Getenv("ADDRESS")
-	if addrEnv != "" {
-		addr = addrEnv
-	} else {
-		flag.StringVar(&addr, "a", "localhost:8080", "Адрес HTTP-сервера")
-		if _, err := url.Parse(addr); err != nil {
-			fmt.Printf("Ошибка: неверный формат адреса сервера в модуле агента: %s\n", addr)
-			return
-		}
-	}
-	// Проверка валидности адреса
-
-	reportSecondsEnv := os.Getenv("REPORT_INTERVAL")
-	if reportSecondsEnv != "" {
-		reportSeconds, _ = strconv.Atoi(reportSecondsEnv)
-	} else {
-		flag.IntVar(&reportSeconds, "r", 10, "Частота отправки метрик на сервер (в секундах)")
-		if reportSeconds <= 0 {
-			fmt.Println("Частота отправки метрик должна быть положительным числом.")
-			flag.Usage()
-			os.Exit(1)
-		}
-
+	config := internal.InitAgentConfig()
+	if config == nil {
+		return // Если возникли ошибки при инициализации конфигурации, выходим
 	}
 
-	pollSecondsEnv := os.Getenv("POLL_INTERVAL")
-	if pollSecondsEnv != "" {
-		pollSeconds, _ = strconv.Atoi(pollSecondsEnv)
-	} else {
-		flag.IntVar(&pollSeconds, "p", 2, "Частота опроса метрик из пакета runtime (в секундах)")
-		if pollSeconds <= 0 {
-			fmt.Println("Частота опроса метрик должна быть положительным числом.")
-			flag.Usage()
-			os.Exit(1)
-		}
-	}
+	// Используем параметры из конфигурации
+	pollInterval := time.Duration(config.PollInterval) * time.Second
+	reportInterval := time.Duration(config.ReportInterval) * time.Second
 
-	flag.Parse()
+	metricsChan := internal.CollectMetrics(pollInterval, config.Address)
 
-	pollInterval := time.Duration(pollSeconds) * time.Second
-	reportInterval := time.Duration(reportSeconds) * time.Second
-
-	metricsChan := internal.CollectMetrics(pollInterval, addr)
-
-	// Горутина  отправки метрик на сервер с интервалом в reportInterval секунд
+	// Горутина отправки метрик на сервер с интервалом в reportInterval секунд
 	go func() {
 		for range time.Tick(reportInterval) {
 			metrics := <-metricsChan
-			sendDataToServer(metrics, addr)
+			sendDataToServer(metrics, config.Address)
 		}
 	}()
 
