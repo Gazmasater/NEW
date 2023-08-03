1ee6fb7f5a6323af101b123e0a108e01ddb1af4c
Lev <lew1968@list.ru>
2023-08-03 21:09:14 +0300

    iter5

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,49 +1,16 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
-	"net/url"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"project.com/internal"
 )
 
-//func parseAddr() (string, error) {
-// Определение и парсинг флага
-//	var addr string
-
-//	flag.StringVar(&addr, "a", "localhost:8080", "Адрес HTTP-сервера")
-
-//	fmt.Println("here is address server", addr)
-
-//	return addr, nil
-//}
-
 func main() {
-
-	var addr string
-
-	// Чтение переменной окружения или установка значения по умолчанию
-	addrEnv := os.Getenv("ADDRESS")
-	println("addr = addrEnv", addrEnv)
-
-	if addrEnv != "" {
-		addr = addrEnv
-	} else {
-		flag.StringVar(&addr, "a", "localhost:8080", "Адрес HTTP-сервера")
-
-		if _, err := url.Parse(addr); err != nil {
-			fmt.Printf("Ошибка: неверный формат адреса сервера: %s\n", addr)
-			flag.Usage()
-			os.Exit(1)
-		}
-
-	}
-
-	flag.Parse()
+	// Инициализируем конфигурацию сервера
+	serverCfg := internal.InitServerConfig()
 
 	gin.SetMode(gin.ReleaseMode)
 
@@ -51,26 +18,18 @@ func main() {
 
 	storage := internal.NewMemStorage()
 
-	// Пример сохранения метрик для демонстр
-	// Пример сохранения метрик для демонстр
+	// Пример сохранения метрик для демонстрации
 	storage.SaveMetric("gauge", "temperature", 25.0)
 	storage.SaveMetric("counter", "requests", int64(10))
 	storage.SaveMetric("counter", "", int64(10))
 
 	r.GET("/metrics", internal.HandleMetrics(storage))
-
 	r.POST("/update/:metricType/:metricName/:metricValue", internal.HandleUpdate(storage))
-
 	r.GET("/value/:metricType/:metricName", internal.HandleUpdate(storage))
 
 	// Запуск HTTP-сервера на указанном адресе
-
-	println("serverURL  main server", addr)
-
-	fmt.Printf("Запуск HTTP-сервера на адресе: %s\n", addr)
-	if err := r.Run(addr); err != nil {
-
+	fmt.Printf("Запуск HTTP-сервера на адресе: %s\n", serverCfg.Address)
+	if err := r.Run(serverCfg.Address); err != nil {
 		log.Fatalf("Ошибка при запуске HTTP-сервера: %s", err)
 	}
-
 }
