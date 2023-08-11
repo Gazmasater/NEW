e1971194f6599b0d2c4326c333627b4a823b357e
Lev <lew1968@list.ru>
2023-08-11 23:54:00 +0300

    midlw iter5

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	"net/http"
-	"strings"
-
-	"github.com/go-chi/chi"
-
 	"project.com/internal/serverin"
 )
 
@@ -18,51 +13,7 @@ func main() {
 
 	deps := serverin.NewHandlerDependencies(storage, logger)
 
-	r := newRouter(deps)
+	r := serverin.NewRouter(deps)
 
 	serverin.StartServer(serverCfg.Address, r)
-}
-
-func UpdateMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.HasPrefix(r.URL.Path, "/update/") {
-			http.Error(w, "StatusBadRequest no update", http.StatusBadRequest)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func ValueMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.HasPrefix(r.URL.Path, "/value/") {
-			http.Error(w, "StatusNotFound", http.StatusNotFound)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
-}
-
-func newRouter(deps *serverin.HandlerDependencies) http.Handler {
-	r := chi.NewRouter()
-
-	r.Get("/metrics", serverin.HandleMetrics(deps))
-
-	// Монтирование подмаршрута /update
-	updateRouter := chi.NewRouter()
-	updateRouter.Use(UpdateMiddleware)
-	updateRouter.Post("/{metricType}/{metricName}/{metricValue}", func(w http.ResponseWriter, r *http.Request) {
-		serverin.HandlePostRequest(w, r, deps)
-	})
-	r.Mount("/update", updateRouter)
-
-	// Монтирование подмаршрута /value
-	valueRouter := chi.NewRouter()
-	valueRouter.Use(ValueMiddleware)
-	valueRouter.Get("/{metricType}/{metricName}", func(w http.ResponseWriter, r *http.Request) {
-		serverin.HandleGetRequest(w, r, deps)
-	})
-	r.Mount("/value", valueRouter)
-
-	return r
 }
