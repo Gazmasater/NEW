3a49ea63f2e6dabdeabf3b46c1f5eb397a2142fa
Lev <lew1968@list.ru>
2023-08-13 18:37:59 +0300

    midlw iter5проверить

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -3,13 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/go-chi/chi"
 	"project.com/internal/serverin"
 )
 
 func main() {
 	// Инициализируем конфигурацию сервера
 	serverCfg := serverin.InitServerConfig()
-	log.Println("serverCfg", serverCfg)
+	log.Println("main serverCfg", serverCfg)
 
 	storage := serverin.NewMemStorage()
 	logger := serverin.NewLogger()
@@ -17,6 +18,7 @@ func main() {
 	deps := serverin.NewHandlerDependencies(storage, logger)
 	controller := serverin.NewMyController(deps) // Создаем новый контроллер
 
-	rootRouter := controller.Route() // Получаем роутер из контроллера
-	serverin.StartServer("localhost:8080", rootRouter)
+	rootRouter := chi.NewRouter()                      // Создаем корневой роутер
+	rootRouter.Mount("/", controller.Route())          // Подключаем роутер из контроллера к корневому роутеру
+	serverin.StartServer("localhost:8080", rootRouter) // Запускаем сервер
 }
