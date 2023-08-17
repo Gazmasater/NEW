7b650018e702c1f51d3a34690070334f222b368e
Lev <lew1968@list.ru>
2023-08-18 00:48:52 +0300

    zanovo iter5

--- internal/serverin/serverfunc.go
+++ internal/serverin/serverfunc.go
@@ -6,10 +6,45 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/go-chi/chi"
 	"go.uber.org/zap"
 )
+
+var sugar zap.SugaredLogger
+
+// WithLogging добавляет дополнительный код для регистрации сведений о запросе
+// и возвращает новый http.Handler.
+func WithLogging(h http.Handler) http.Handler {
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		// функция Now() возвращает текущее время
+		start := time.Now()
+
+		// эндпоинт /ping
+		uri := r.RequestURI
+		// метод запроса
+		method := r.Method
+
+		// точка, где выполняется хендлер pingHandler
+		h.ServeHTTP(w, r) // обслуживание оригинального запроса
+
+		// Since возвращает разницу во времени между start
+		// и моментом вызова Since. Таким образом можно посчитать
+		// время выполнения запроса.
+		duration := time.Since(start)
+
+		// отправляем сведения о запросе в zap
+		sugar.Infoln(
+			"uri", uri,
+			"method", method,
+			"duration", duration,
+		)
+
+	}
+	// возвращаем функционально расширенный хендлер
+	return http.HandlerFunc(logFn)
+}
 
 func (mc *HandlerDependencies) handlePostRequest(w http.ResponseWriter, r *http.Request) {
 	// Обработка POST-запроса
@@ -155,6 +190,7 @@ func (mc *HandlerDependencies) handleMetrics(w http.ResponseWriter, r *http.Requ
 func (mc *HandlerDependencies) Route() *chi.Mux {
 
 	r := chi.NewRouter()
+	r.Use(WithLogging)
 
 	r.Get("value/{metricType}/{metricName}", mc.handleGetRequest)
 
