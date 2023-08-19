0c8b6a28bc555896ecd297b384046f6c91b49bfe
Lev <lew1968@list.ru>
2023-08-19 22:43:43 +0300

    iter7

--- internal/serverin/serverfunc.go
+++ internal/serverin/serverfunc.go
@@ -15,11 +15,6 @@ import (
 
 var Sugar *zap.SugaredLogger
 
-func logFn(w http.ResponseWriter, r *http.Request) {
-	Sugar.Info("logFn is called")
-	// Ваш код для логирования здесь
-}
-
 // InitLogger инициализирует логгер для использования в WithLogging.
 func InitLogger() {
 	logger, err := zap.NewDevelopment()
@@ -30,23 +25,14 @@ func InitLogger() {
 }
 
 func WithLogging(h http.Handler) http.Handler {
-	// Объявление переменной logger
-
-	logger, err := zap.NewDevelopment()
-	if err != nil {
-		panic(err)
-	}
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		Sugar.Info("WithLogging!!!!")
-
+	logFn := func(w http.ResponseWriter, r *http.Request) {
 		// функция Now() возвращает текущее время
 		start := time.Now()
 
 		uri := r.RequestURI
-		// метод запроса
+
 		method := r.Method
-		Sugar.Debug("WithLogging method", zap.String("method", method))
+		Sugar.Debugw("WithLogging method", zap.String("method", method))
 
 		// создаем ResponseRecorder, чтобы получить доступ к коду статуса и размеру ответа
 		rr := httptest.NewRecorder()
@@ -58,23 +44,26 @@ func WithLogging(h http.Handler) http.Handler {
 		statusCode := rr.Code
 		responseSize := rr.Body.Len()
 
-		// Since возвращает разницу во времени между start
-		// и моментом вызова Since. Таким образом можно посчитать
-		// время выполнения запроса.
 		duration := time.Since(start)
 
-		// Инициализация логгера Zap
+		// отправляем сведения о запросе в zap
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			// Обработка ошибки
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
 
-		// Используем logger.Sugar() для удобной работы с логгером
-		sugar := logger.Sugar()
-
-		sugar.Infow(
+		Sugar.Infow(
 			"uri", uri,
 			"method", method,
 			"duration", duration,
-			"statusCode", strconv.Itoa(statusCode),
-			"responseSize", strconv.Itoa(responseSize),
+			"statusCode", strconv.Itoa(statusCode), // Преобразование числового значения в строку
+			"responseSize", strconv.Itoa(responseSize), // Преобразование числового значения в строку
 		)
+
+		// Закрываем логгер после использования
+		defer logger.Sync()
 
 		// копируем данные из ResponseRecorder в http.ResponseWriter
 		for k, v := range rr.Header() {
@@ -82,8 +71,7 @@ func WithLogging(h http.Handler) http.Handler {
 		}
 		w.WriteHeader(statusCode)
 		w.Write(rr.Body.Bytes())
-	})
-
+	}
 	// возвращаем функционально расширенный хендлер
 	return http.HandlerFunc(logFn)
 }
@@ -94,7 +82,7 @@ func (mc *HandlerDependencies) handlePostRequest(w http.ResponseWriter, r *http.
 	metricType := chi.URLParam(r, "metricType")
 	metricName := chi.URLParam(r, "metricName")
 	metricValue := chi.URLParam(r, "metricValue")
-	println("metricType....", metricType, metricName, metricValue)
+	mc.Logger.Info("metricType", zap.String("metricType", metricType), zap.String("metricName", metricName), zap.String("metricValue", metricValue))
 
 	if metricType != "gauge" && metricType != "counter" {
 		http.Error(w, "StatusBadRequest", http.StatusBadRequest)
