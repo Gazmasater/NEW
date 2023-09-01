dfb9f854beda267394124c4cfa2c5d50e0d35612
Gazmasater <dkot@Home-PC>
2023-09-01 21:38:42 +0300

    iter7 json+text

--- internal/agentin.go
+++ internal/agentin.go
@@ -5,7 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"math/rand"
+
+	//	"math/rand"
 	"net/http"
 	"runtime"
 	"time"
@@ -26,7 +27,7 @@ func CollectMetrics(pollInterval time.Duration, serverURL string) <-chan []*Metr
 	metricsChan := make(chan []*Metrics)
 	println("CollectMetrics serverURL string", serverURL)
 	// Переменная для счетчика обновлений метрик
-	var pollCount int64 = 0
+	//var pollCount int64 = 0
 
 	var memStats runtime.MemStats
 	go func() {
@@ -35,71 +36,71 @@ func CollectMetrics(pollInterval time.Duration, serverURL string) <-chan []*Metr
 		for {
 			runtime.ReadMemStats(&memStats)
 
-			allocValue := float64(memStats.Alloc)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "Alloc", Value: &allocValue})
+			// allocValue := float64(memStats.Alloc)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "Alloc", Value: &allocValue})
 
-			buckHashSysValue := float64(memStats.BuckHashSys)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "BuckHashSys", Value: &buckHashSysValue})
-			freesValue := float64(memStats.Frees)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "Frees", Value: &freesValue})
+			// buckHashSysValue := float64(memStats.BuckHashSys)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "BuckHashSys", Value: &buckHashSysValue})
+			// freesValue := float64(memStats.Frees)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "Frees", Value: &freesValue})
 			gCCPUFractionValue := float64(memStats.GCCPUFraction)
 			metrics = append(metrics, &Metrics{MType: "gauge", ID: "GCCPUFraction", Value: &gCCPUFractionValue})
-			gCSysValue := float64(memStats.GCSys)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "GCSys", Value: &gCSysValue})
-			heapAllocValue := float64(memStats.HeapAlloc)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapAlloc", Value: &heapAllocValue})
-			heapIdleValue := float64(memStats.HeapIdle)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapIdle", Value: &heapIdleValue})
-			heapInuseValue := float64(memStats.HeapInuse)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapInuse", Value: &heapInuseValue})
-			heapObjectsValue := float64(memStats.HeapObjects)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapObjects", Value: &heapObjectsValue})
-			heapReleasedValue := float64(memStats.HeapReleased)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapReleased", Value: &heapReleasedValue})
-			heapSysValue := float64(memStats.HeapSys)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapSys", Value: &heapSysValue})
-			lastGCValue := float64(memStats.LastGC)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "LastGC", Value: &lastGCValue})
-			lookupsValue := float64(memStats.Lookups)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "Lookups", Value: &lookupsValue})
-			mCacheInuseValue := float64(memStats.MCacheInuse)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "MCacheInuse", Value: &mCacheInuseValue})
-			mCacheSysValue := float64(memStats.MCacheSys)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "MCacheSys", Value: &mCacheSysValue})
-			mSpanInuseValue := float64(memStats.MSpanInuse)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "MSpanInuse", Value: &mSpanInuseValue})
-			mSpanSysValue := float64(memStats.MSpanSys)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "MSpanSys", Value: &mSpanSysValue})
-			mallocsValue := float64(memStats.Mallocs)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "Mallocs", Value: &mallocsValue})
-			nextGCValue := float64(memStats.NextGC)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "NextGC", Value: &nextGCValue})
-			numForcedGCValue := float64(memStats.NumForcedGC)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "NumForcedGC", Value: &numForcedGCValue})
-			numGCValue := float64(memStats.NumGC)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "NumGC", Value: &numGCValue})
-			otherSysValue := float64(memStats.OtherSys)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "OtherSys", Value: &otherSysValue})
-			pauseTotalNsValue := float64(memStats.PauseTotalNs)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "PauseTotalNs", Value: &pauseTotalNsValue})
-			stackInuseValue := float64(memStats.StackInuse)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "StackInuse", Value: &stackInuseValue})
-			stackSysValue := float64(memStats.StackSys)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "StackSys", Value: &stackSysValue})
-			sysValue := float64(memStats.Sys)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "Sys", Value: &sysValue})
-			totalAllocValue := float64(memStats.TotalAlloc)
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "TotalAlloc", Value: &totalAllocValue})
+			// gCSysValue := float64(memStats.GCSys)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "GCSys", Value: &gCSysValue})
+			// heapAllocValue := float64(memStats.HeapAlloc)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapAlloc", Value: &heapAllocValue})
+			// heapIdleValue := float64(memStats.HeapIdle)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapIdle", Value: &heapIdleValue})
+			// heapInuseValue := float64(memStats.HeapInuse)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapInuse", Value: &heapInuseValue})
+			// heapObjectsValue := float64(memStats.HeapObjects)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapObjects", Value: &heapObjectsValue})
+			// heapReleasedValue := float64(memStats.HeapReleased)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapReleased", Value: &heapReleasedValue})
+			// heapSysValue := float64(memStats.HeapSys)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "HeapSys", Value: &heapSysValue})
+			// lastGCValue := float64(memStats.LastGC)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "LastGC", Value: &lastGCValue})
+			// lookupsValue := float64(memStats.Lookups)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "Lookups", Value: &lookupsValue})
+			// mCacheInuseValue := float64(memStats.MCacheInuse)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "MCacheInuse", Value: &mCacheInuseValue})
+			// mCacheSysValue := float64(memStats.MCacheSys)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "MCacheSys", Value: &mCacheSysValue})
+			// mSpanInuseValue := float64(memStats.MSpanInuse)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "MSpanInuse", Value: &mSpanInuseValue})
+			// mSpanSysValue := float64(memStats.MSpanSys)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "MSpanSys", Value: &mSpanSysValue})
+			// mallocsValue := float64(memStats.Mallocs)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "Mallocs", Value: &mallocsValue})
+			// nextGCValue := float64(memStats.NextGC)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "NextGC", Value: &nextGCValue})
+			// numForcedGCValue := float64(memStats.NumForcedGC)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "NumForcedGC", Value: &numForcedGCValue})
+			// numGCValue := float64(memStats.NumGC)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "NumGC", Value: &numGCValue})
+			// otherSysValue := float64(memStats.OtherSys)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "OtherSys", Value: &otherSysValue})
+			// pauseTotalNsValue := float64(memStats.PauseTotalNs)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "PauseTotalNs", Value: &pauseTotalNsValue})
+			// stackInuseValue := float64(memStats.StackInuse)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "StackInuse", Value: &stackInuseValue})
+			// stackSysValue := float64(memStats.StackSys)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "StackSys", Value: &stackSysValue})
+			// sysValue := float64(memStats.Sys)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "Sys", Value: &sysValue})
+			// totalAllocValue := float64(memStats.TotalAlloc)
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "TotalAlloc", Value: &totalAllocValue})
 
-			// // Добавляем метрику RandomValue типа gauge с произвольным значением
-			randomValue := rand.Float64()
-			metrics = append(metrics, &Metrics{MType: "gauge", ID: "RandomValue", Value: &randomValue})
+			// // // Добавляем метрику RandomValue типа gauge с произвольным значением
+			// randomValue := rand.Float64()
+			// metrics = append(metrics, &Metrics{MType: "gauge", ID: "RandomValue", Value: &randomValue})
 
-			// Добавляем метрику PollCount типа counter!!
-			metrics = append(metrics, &Metrics{MType: "counter", ID: "PollCount", Delta: &pollCount})
+			// // Добавляем метрику PollCount типа counter!!
+			// metrics = append(metrics, &Metrics{MType: "counter", ID: "PollCount", Delta: &pollCount})
 
-			// Увеличиваем счетчик обновлений метр!!!
-			pollCount++
+			// // Увеличиваем счетчик обновлений метр!!!
+			// pollCount++
 
 			metricsChan <- metrics
 			time.Sleep(pollInterval)
@@ -140,7 +141,8 @@ func SendDataToServer(metrics []*Metrics, serverURL string) {
 			fmt.Println("Ошибка при сериализации данных в JSON", err)
 			return
 		}
-		//fmt.Println("Сериализированные данные в JSON:", string(jsonData))
+
+		fmt.Println("Сериализированные данные в JSON:", string(jsonData))
 
 		serverURL := fmt.Sprintf("http://%s/update/%s/%s/%v", serverURL, metric.MType, metric.ID, metricValue)
 		//	println("SendDataToServer serverURL", serverURL)
@@ -176,7 +178,7 @@ func SendDataToServer(metrics []*Metrics, serverURL string) {
 		}
 
 		// Вывод тела ответа на экран
-		//fmt.Println("Тело ответа сервера:", string(responseBody))
+		fmt.Println("Тело ответа сервера:", string(responseBody))
 
 		if resp.StatusCode == http.StatusOK {
 			// Чтение и обработка ответа
@@ -215,7 +217,7 @@ func SendServerValue(metrics []*Metrics, serverURL string) {
 
 		serverURL := fmt.Sprintf("http://%s/value/%s/%s", serverURL, metric.MType, metric.ID)
 
-		println("SendServerValue serverURL", serverURL)
+		//println("SendServerValue serverURL", serverURL)
 		req, err := http.NewRequest("POST", serverURL, bytes.NewBuffer(jsonData))
 		if err != nil {
 			fmt.Println("Ошибка при создании запроса:", err)
