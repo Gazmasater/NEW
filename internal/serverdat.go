9ff735432167642538221c77908a95be66a6c90a
Gazmasater <dkot@Home-PC>
2023-10-20 11:51:47 +0300

    iter12

--- internal/serverdat.go
+++ internal/serverdat.go
@@ -20,7 +20,7 @@ func ParseAddr() (string, error) {
 }
 
 type Metrics struct {
-	ID    string   `json:"id"`              // имя метрики
+	ID    string   `json:"id"`
 	MType string   `json:"type"`            // параметр, принимающий значение gauge или counter
 	Delta *int64   `json:"delta,omitempty"` // значение метрики в случае передачи counter
 	Value *float64 `json:"value,omitempty"` // значение метрики в случае передачи gauge
