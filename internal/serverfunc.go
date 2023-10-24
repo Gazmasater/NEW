6ddbc2829699b904ac342c7b1e5ee6c6d266bcb8
Gazmasater <dkot@Home-PC>
2023-10-24 17:01:17 +0300

    iter11

--- internal/serverfunc.go
+++ internal/serverfunc.go
@@ -58,6 +58,7 @@ func isInteger(s string) bool {
 	return err == nil
 }
 
+// хендлер post путь /update/
 func (mc *HandlerDependencies) HandlePostRequest(w http.ResponseWriter, r *http.Request) {
 
 	contentType := r.Header.Get("Content-Type")
