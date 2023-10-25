22e2ad8bb0d065b746e45966903e14e30e1126d0
Gazmasater <dkot@Home-PC>
2023-10-25 23:11:12 +0300

    iter13

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-
+	"sync"
 	"time"
 
 	"project.com/internal"
@@ -11,24 +11,59 @@ import (
 func main() {
 	config := internal.InitAgentConfig()
 	if config == nil {
-		return // Если возникли ошибки при инициализации конфигурации, выходим
+		return
 	}
 	internal.Init()
-	// Используем параметры из конфигурации
+
 	pollInterval := time.Duration(config.PollInterval) * time.Second
 	reportInterval := time.Duration(config.ReportInterval) * time.Second
 
 	metricsChan := internal.CollectMetrics(pollInterval, config.Address)
 
-	// Горутина отправки метрик на сервер с интервалом в reportInterval секунд
+	var mu sync.Mutex
+	var bufferedMetrics []*internal.Metrics
+
+	immediateMu := sync.Mutex{} // Используем {} для создания экземпляра мьютекса
+
 	go func() {
 		for range time.Tick(reportInterval) {
 			metrics := <-metricsChan
-			internal.SendDataToServer(metrics, config.Address) //post запрос по пути /update/
+
+			// Блокируем доступ к bufferedMetrics при немедленной отправке
+			immediateMu.Lock()
+			internal.SendDataToServer(metrics, config.Address)
+			immediateMu.Unlock()
 		}
 	}()
 
-	for range time.Tick(pollInterval) {
-		fmt.Println("Сбор метрик...")
+	go func() {
+		for range time.Tick(reportInterval) {
+			mu.Lock()
+			metricsToSend := make([]*internal.Metrics, len(bufferedMetrics))
+			copy(metricsToSend, bufferedMetrics)
+			bufferedMetrics = nil
+			mu.Unlock()
+
+			if len(metricsToSend) > 0 { // Проверка на пустой срез
+
+				err := internal.SendDataToServerBatch(metricsToSend, config.Address)
+				if err != nil {
+					fmt.Println("Ошибка при отправке метрик на сервер:", err)
+				}
+			}
+		}
+	}()
+
+	for {
+		select {
+		case metrics := <-metricsChan:
+			fmt.Println("Сбор метрик...")
+
+			mu.Lock()
+			bufferedMetrics = append(bufferedMetrics, metrics...)
+			mu.Unlock()
+		case <-time.After(pollInterval):
+			// Выполняйте другие задачи, если не получены новые метрики в течение pollInterval
+		}
 	}
 }
