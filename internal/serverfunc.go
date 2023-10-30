87bb4c5122c30d877c3e51d97b87cad511d2d253
Gazmasater <dkot@Home-PC>
2023-10-31 02:16:19 +0300

    iter14

--- internal/serverfunc.go
+++ internal/serverfunc.go
@@ -8,21 +8,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// func Init() {
-// 	// Инициализация логгера
-// 	var err error
-// 	logger, err = zap.NewProduction()
-// 	if err != nil {
-// 		panic("failed to initialize logger")
-// 	}
-// 	defer logger.Sync() // flushes buffer, if any
-// }
-
-type Metric struct {
-	Name  string
-	Value interface{}
-}
-
 type GzipResponseWriter struct {
 	http.ResponseWriter
 	Writer *gzip.Writer
