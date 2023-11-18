116f9ec5bd4756a10a1ae149b95437eb0cacc423
Gazmasater <dkot@Home-PC>
2023-11-18 22:24:19 +0300

    iter14

--- internal/collector/collector.go
+++ internal/collector/collector.go
@@ -1,19 +1,20 @@
 package collector
 
 import (
+	"fmt"
 	"math/rand"
 	"runtime"
 	"time"
 
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
 	"project.com/internal/models"
 )
 
 func CollectMetrics(pollInterval time.Duration, serverURL string) <-chan []*models.Metrics {
 	metricsChan := make(chan []*models.Metrics)
-	println("CollectMetrics serverURL string", serverURL)
-
 	var pollCount int64 = 0
-
+	println("CollectMetrics serverURL", serverURL)
 	var memStats runtime.MemStats
 
 	go func() {
@@ -127,4 +128,24 @@ func CollectMetrics(pollInterval time.Duration, serverURL string) <-chan []*mode
 	}()
 
 	return metricsChan
+}
+
+func CollectAdditionalMetrics() (float64, float64, []float64) {
+	vmStat, err := mem.VirtualMemory()
+	if err != nil {
+		fmt.Println("Ошибка при получении информации о памяти:", err)
+
+		return 0, 0, nil
+	}
+
+	cpuInfo, err := cpu.Percent(time.Second, false)
+	if err != nil {
+		fmt.Println("Ошибка при получении информации о CPU:", err)
+		return 0, 0, nil
+	}
+
+	// Преобразуйте vmStat.Total в float64
+	totalMemory := float64(vmStat.Total)
+
+	return totalMemory, float64(vmStat.Free), cpuInfo
 }
