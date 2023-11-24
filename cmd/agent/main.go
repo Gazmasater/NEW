d13ae311a96421a25a77415f16820e6b8eea9941
master <master@Home-PC>
2023-11-25 00:56:20 +0300

    iter15

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -8,28 +8,60 @@ import (
 	"project.com/internal/collector"
 	"project.com/internal/config"
 	"project.com/internal/logger"
+	"project.com/internal/models"
 	"project.com/internal/sender"
 )
 
 func main() {
 	cfg := config.Must(config.New())
 
-	_, err := logger.New()
+	log, err := logger.New()
 	if err != nil {
 
 		fmt.Printf("Ошибка при создании логгера: %s\n", err)
 		return
 	}
-	// Используем параметры из конфигурации
+
 	pollInterval := time.Duration(cfg.PollInterval) * time.Second
 	reportInterval := time.Duration(cfg.ReportInterval) * time.Second
 
 	metricsChan := collector.CollectMetrics(pollInterval, cfg.Address)
+	metricsChanTotal := make(chan models.SysMetrics)
 
+	requestChannel := make(chan struct{}, cfg.RateLimit)
+
+	// Горутина отправки метрик на сервер
 	go func() {
 		for range time.Tick(reportInterval) {
-			metrics := <-metricsChan
-			sender.SendDataToServer(metrics, cfg.Address) //post запрос по пути /update/
+			select {
+			case metrics := <-metricsChan:
+
+				select {
+				case requestChannel <- struct{}{}:
+					go func() {
+						sender.SendDataToServer(metrics, cfg.Address) // POST-запрос по пути /update/
+						<-requestChannel
+					}()
+				default:
+
+					log.Info("Превышен лимит одновременных запросов, метрика будет отправлена в следующем цикле.")
+				}
+			case metrics := <-metricsChanTotal:
+				sender.SendSysMetricsToServer(metrics, cfg.Address)
+			}
+		}
+	}()
+
+	go func() {
+		for range time.Tick(pollInterval) {
+			totalMemory, freeMemory, cpuUtilization := collector.CollectAdditionalMetrics()
+			metrics := models.SysMetrics{
+				TotalMemory:    totalMemory,
+				FreeMemory:     freeMemory,
+				CPUUtilization: cpuUtilization,
+			}
+
+			metricsChanTotal <- metrics
 		}
 	}()
 
