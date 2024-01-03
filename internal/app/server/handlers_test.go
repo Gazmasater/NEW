d9ace4d9717c20e098f7bcfd62ced5b9bb8d3bdf
putilin.lev <vash-agent48@yandex.ru>
2024-01-04 01:21:02 +0300

    iter14

--- internal/app/server/handlers_test.go
+++ internal/app/server/handlers_test.go
@@ -1,24 +1,55 @@
 package server
 
 import (
+	"bytes"
 	"database/sql"
+	"log"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
-	_ "github.com/denisenkom/go-mssqldb"
-	_ "github.com/mattn/go-sqlite3"
+	"project.com/internal/config"
 )
 
-func TestDBConnection(t *testing.T) {
-	// Создание временной базы данных SQLite для тестирования подключения
-	db, err := sql.Open("sqlite3", ":memory:")
+func TestApp_updateHandlerJSON(t *testing.T) {
+
+	db, err := sql.Open("postgres", mc.Config.DatabaseDSN)
 	if err != nil {
-		t.Fatalf("error '%s' was not expected when opening a database connection", err)
+		log.Printf("Ошибка при открытии базы данных: %v", err)
+		return err
 	}
 	defer db.Close()
+	// Подготовка фейковых данных для тестов
+	mockStorage := storage.NewMemStorage() // Используйте вашу реализацию хранилища
+	mockLogger := zap.NewExample()
+	mockConfig := &config.ServerConfig{} // Подставьте реальную конфигурацию
 
-	// Проверка подключения к базе данных
-	err = db.Ping()
-	if err != nil {
-		t.Fatalf("error connecting to the database: %s", err)
+	// Создание фейкового HTTP запроса
+	req := httptest.NewRequest("POST", "/update/", bytes.NewBufferString(`{"id": "test1", "type": "counter", "delta": 10}`))
+	rr := httptest.NewRecorder()
+
+	// Создание экземпляра вашего приложения
+	mc := &app{
+		Storage: mockStorage,
+		Logger:  mockLogger,
+		Config:  mockConfig,
+		DB:      db, // Добавьте вашу базу данных, если используете
 	}
+
+	// Вызов функции обработчика
+	mc.updateHandlerJSON(rr, req)
+
+	// TODO: Добавьте утверждения, чтобы проверить ожидаемое поведение в ответ на запрос.
+	// Проверьте, что метрика была успешно обработана или что была возвращена ошибка в случае неудачи.
+	// Например:
+	// - Проверка статус кода ответа (rr.Code)
+	// - Проверка содержимого ответа (rr.Body)
+	// - Проверка изменений в вашем хранилище или файлах
+
+	// Пример утверждения:
+	if rr.Code != http.StatusOK {
+		t.Errorf("Expected status OK, got: %d", rr.Code)
+	}
+
+	// ... Добавьте другие утверждения по вашему усмотрению
 }
